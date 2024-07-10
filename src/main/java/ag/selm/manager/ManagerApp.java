77ee4cc02b65af0b0c9c296b1540708e55cba5a1
IT-Schnitzel <oficealex@mail.ru>
2024-07-10 16:39:49 +0300

    t

---
+++ src/main/java/ag/selm/manager/ManagerApp.java
@@ -0,0 +1,11 @@
+package ag.selm.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApp.class, args);
+    }
+}
