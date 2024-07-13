5c69ec7d1e47619efcae2f45d27a74473df1c139
IT-Schnitzel <oficealex@mail.ru>
2024-07-13 16:18:36 +0300

    t

--- src/main/java/ag/selm/manager/controller/payload/UpdateProductPayload.java
+++ src/main/java/ag/selm/manager/controller/payload/UpdateProductPayload.java
@@ -1,5 +1,13 @@
 package ag.selm.manager.controller.payload;
 
-public record UpdateProductPayload(String title, String details){
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UpdateProductPayload(
+        @NotNull
+        @Size(min =  3, max = 30)
+        String title,
+        @Size(max = 500)
+        String details){
 
 }
