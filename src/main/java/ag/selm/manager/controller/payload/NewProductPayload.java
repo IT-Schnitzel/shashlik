48ee8dc4a43aaf4714a1db809a4ff9eee89b9ee1
IT-Schnitzel <oficealex@mail.ru>
2024-07-13 16:34:07 +0300

    t

--- src/main/java/ag/selm/manager/controller/payload/NewProductPayload.java
+++ src/main/java/ag/selm/manager/controller/payload/NewProductPayload.java
@@ -4,10 +4,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record NewProductPayload(
-        @NotNull
-        @Size(min =  3, max = 30)
+        @NotNull(message = "{catalogue.products.edit.errors.title_size_is_null}")
+        @Size(min =  3, max = 30, message = "{catalogue.products.edit.errors.title_size_is_invalid}")
         String title,
-        @Size(max = 500)
+        @Size(max = 500, message = "{catalogue.products.edit.errors.details_size_is_invalid}")
         String details){
-
 }
--- src/main/java/ag/selm/manager/controller/payload/UpdateProductPayload.java
+++ src/main/java/ag/selm/manager/controller/payload/UpdateProductPayload.java
@@ -4,10 +4,10 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UpdateProductPayload(
-        @NotNull
-        @Size(min =  3, max = 30)
+        @NotNull(message = "{catalogue.products.create.errors.title_size_is_null}")
+        @Size(min =  3, max = 30, message = "{catalogue.products.create.errors.title_size_is_invalid}")
         String title,
-        @Size(max = 500)
+        @Size(max = 500, message = "{catalogue.products.create.errors.details_size_is_invalid}")
         String details){
 
 }
